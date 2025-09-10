47566d73dcc39ff43610946bcb38b10410eb0720
Naomi <naomibuadee1@gmail.com>
2025-09-10 15:28:29 +0100

    (Query): Add Flight Query

---
+++ backend/api/graphql/queries/flight_booking_queries.py
@@ -0,0 +1,15 @@
+import graphene
+from graphene_django import DjangoObjectType
+from ..types.flight_booking_type import FlightBookingType
+from ...models.flight_booking import FlightBooking
+from ...models.city import City
+
+
+class FlightBookingQueries(graphene.ObjectType):
+    flight_bookings = graphene.List(
+        FlightBookingType,
+        user_id = graphene.Int(required=True)
+        )
+
+    def resolve_flight_bookings(self,info,user_id):
+        return FlightBooking.objects.filter(user__id=user_id)
