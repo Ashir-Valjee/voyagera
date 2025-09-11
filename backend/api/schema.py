be0ac090519a46b815d7b53028cf51b1ff3a93a8
Ken <kenlumumba@gmail.com>
2025-09-11 12:26:15 +0100

    (Mutation): create flight booking mutation

--- backend/api/schema.py
+++ backend/api/schema.py
@@ -5,11 +5,12 @@ from api.graphql.queries.city_queries import CityQueries
 from api.graphql.queries.flight_booking_queries import FlightBookingQueries
 from api.graphql.queries.activity_booking_queries import ActivityBookingQueries
 from api.graphql.queries.profile_queries import ProfileQueries
+from api.graphql.mutations.flight_booking_mutations import CreateFlightBookingMutation
 
 class Query(UserQueries, CityQueries,FlightBookingQueries, ActivityBookingQueries, ProfileQueries, graphene.ObjectType):
     pass
 
-class Mutation(AuthMutations, graphene.ObjectType):
+class Mutation(AuthMutations, CreateFlightBookingMutation, graphene.ObjectType):
     pass
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
