3ac5313b6adff4a8438a1ecdbfdd5eb545900131
Saima Abdus <saima.abdus@kraken.tech>
2025-09-11 15:19:31 +0100

    (Mutation): Create activity booking mutation

--- backend/api/schema.py
+++ backend/api/schema.py
@@ -6,11 +6,12 @@ from api.graphql.queries.flight_booking_queries import FlightBookingQueries
 from api.graphql.queries.activity_booking_queries import ActivityBookingQueries
 from api.graphql.queries.profile_queries import ProfileQueries
 from api.graphql.mutations.flight_booking_mutations import FlightBookingMutations
+from api.graphql.mutations.activity_booking_mutations import ActivityBookingMutations
 
 class Query(UserQueries, CityQueries,FlightBookingQueries, ActivityBookingQueries, ProfileQueries, graphene.ObjectType):
     pass
 
-class Mutation(AuthMutations, FlightBookingMutations, graphene.ObjectType):
+class Mutation(AuthMutations, FlightBookingMutations, ActivityBookingMutations, graphene.ObjectType):
     pass
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
