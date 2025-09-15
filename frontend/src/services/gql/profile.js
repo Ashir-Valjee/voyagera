88097df51b87145365bb4a5b3a969562c6b7fdbe
Naomi <naomibuadee1@gmail.com>
2025-09-15 14:55:04 +0100

    (Services): Update profile service and updateProfileMutation

--- frontend/src/services/gql/profile.js
+++ frontend/src/services/gql/profile.js
@@ -15,3 +15,47 @@ export const Profile = gql`
   }
 
 `;
+
+export const UpdateProfile = gql`
+  mutation UpdateProfile(
+    $id: ID!
+    $firstName: String
+    $lastName: String
+    $profilePicUrl: String
+    $homeCityId: ID
+    $likesMusic: Boolean
+    $likesSports: Boolean
+    $likesArts: Boolean
+    $likesFilm: Boolean
+    $likesFamily: Boolean
+  ) {
+    updateProfile(
+      id: $id
+      firstName: $firstName
+      lastName: $lastName
+      profilePicUrl: $profilePicUrl
+      homeCityId: $homeCityId
+      likesMusic: $likesMusic
+      likesSports: $likesSports
+      likesArts: $likesArts
+      likesFilm: $likesFilm
+      likesFamily: $likesFamily
+    ) {
+      success
+      errors
+      profile {
+        id
+        likesMusic
+        likesSports
+        likesArts
+        likesFilm
+        likesFamily
+        firstName
+        lastName
+        profilePicUrl
+      }
+    }
+  }
+`;
+
+
