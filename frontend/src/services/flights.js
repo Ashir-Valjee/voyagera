6332456a593b012fc986fa321ef3a4c4cfd5e534
ashir-valjee <ashir@eco-motive.com>
2025-09-15 16:16:39 +0100

    added rest of flight services

--- frontend/src/services/flights.js
+++ frontend/src/services/flights.js
@@ -1,5 +1,12 @@
 import { apollo } from "../lib/apollo";
-import { AMADEUS_FLIGHT_OFFERS } from "./gql/flights";
+import {
+  AMADEUS_FLIGHT_OFFERS,
+  CreateFlightBooking,
+  UpdateFlightBooking,
+  FlightBooking,
+  FlightBookingById,
+  DeleteFlightBooking,
+} from "./gql/flights";
 
 //  * params:
 //  *   origin, destination, departureDate, returnDate?, adults?, nonStop?, maxResults?, currency?
@@ -17,4 +24,67 @@ export async function searchFlights(params) {
   });
 
   return data?.amadeusFlightOffers ?? [];
+}
+
+export async function fetchFlightBookings() {
+  const { data } = await apollo.query({
+    query: FlightBooking,
+    fetchPolicy: "network-only",
+  });
+  return data?.flightBookings ?? null;
+}
+
+export async function fetchFlightBookingById(flightId) {
+  const { data } = await apollo.query({
+    query: FlightBookingById,
+    variables: { flightId },
+    fetchPolicy: "network-only",
+  });
+  return data?.flightBookingById ?? null;
+}
+
+export async function createFlightBooking(
+  arrivalDateTime,
+  departureAirport,
+  departureCityId,
+  departureDateTime,
+  destinationAirport,
+  destinationCityId,
+  flightDuration,
+  numberOfPassengers,
+  numberOfStops,
+  totalPrice
+) {
+  const { data } = await apollo.mutate({
+    mutation: CreateFlightBooking,
+    variables: {
+      arrivalDateTime,
+      departureAirport,
+      departureCityId,
+      departureDateTime,
+      destinationAirport,
+      destinationCityId,
+      flightDuration,
+      numberOfPassengers,
+      numberOfStops,
+      totalPrice,
+    },
+  });
+  return data?.createFlightBooking ?? null;
+}
+
+export async function updateFlightBooking(id, numberOfPassengers, totalPrice) {
+  const { data } = await apollo.mutate({
+    mutation: UpdateFlightBooking,
+    variables: { id, numberOfPassengers, totalPrice },
+  });
+  return data?.updateFlightBooking ?? null;
+}
+
+export async function deleteFlightBooking(id) {
+  const { data } = await apollo.mutate({
+    mutation: DeleteFlightBooking,
+    variables: { id },
+  });
+  return data?.deleteFlightBooking ?? null;
 }
